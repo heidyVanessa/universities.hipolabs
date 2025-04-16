980648576cf10eecdb8b22fe494d51a7d6355c9a
heidyVanessa <130875721+heidyVanessa@users.noreply.github.com>
2025-04-16 21:59:23 +0000

    Cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Paises from './Componentes/Paises'
 import Nombres from './Componentes/Nombres'
@@ -9,13 +10,18 @@ import Dominios from './Componentes/Dominios'
 function App() {
 
   return (
-    <>
-    <Paises />
-    <Nombres />
-    <Menu />
-    <Lista />
-    <Dominios />
-    </>
+    <Router>
+      
+      <Menu />
+
+      <Routes>
+        <Route path="/" element={<Paises />} />
+        <Route path="/" element={<Nombres />} />
+        <Route path="/" element={<Menu />} />
+        <Route path="/" element={<Lista />} />
+        <Route path="/" element={<Dominios />} />
+      </Routes>
+    </Router>  
   )
 }
 
--- src/Componentes/Menu/index.jsx
+++ src/Componentes/Menu/index.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './style.css'
 
 function Menu() {
   
     return (
-      <h1>Menu</h1>
+        <nav className="c-menu">
+            <Link to="/">Lista</Link>
+            <Link to="/capturados">Capturados</Link>
+            <Link to="/aleatorios">Aleatorio</Link>
+            <Link to="/usuarios">Usuarios</Link>
+            <Link to="/favoritos">Favoritos</Link>
+        </nav>
     )
   }
   
